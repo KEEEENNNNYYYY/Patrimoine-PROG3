aca71b7ccb7f2f8eec4d71dff6d0fe0faa5afe79
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-11 12:00:00 +0200

    feat : added parents function

--- src/main/java/HEI/model/TrainDeVie.java
+++ src/main/java/HEI/model/TrainDeVie.java
@@ -1,9 +1,15 @@
 package HEI.model;
 
+import java.time.LocalDate;
+
 public final  class TrainDeVie  extends Possession{
 
     public TrainDeVie(String nom, Devise devise, Argent valeur, Double tauxDapreciation) {
         super(nom, devise, valeur, tauxDapreciation);
     }
 
+    @Override
+    public Possession projectionFuture(LocalDate dateFuture) {
+        return null;
+    }
 }
