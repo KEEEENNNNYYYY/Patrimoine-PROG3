4422502684c4df3f788808618132a6515bc699ee
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-18 12:00:00 +0200

    fix : projection future function

--- src/main/java/org/example/models/Compte.java
+++ src/main/java/org/example/models/Compte.java
@@ -3,24 +3,36 @@ package org.example.models;
 import java.time.LocalDate;
 
 public final class Compte extends Possession {
+    private final Set <TrainDeVie> trainDeVies;
+    private final LocalDate dateDeCreation ;
 
-    public Compte(String nomDeLaPossession, LocalDate aDateDe, Argent valeur) {
+    public Compte(String nomDeLaPossession, LocalDate aDateDe, Argent valeur  , LocalDate dateDeCreation) {
         super(nomDeLaPossession, aDateDe, valeur);
+        this.traindeDeVies = new HashSet<>();
+        this.dateDeCreation = dateDeCreation;
     }
 
     @Override
     public Possession projectionFuture(LocalDate dateFuture) {
-        if (dateFuture.isBefore(this.getADateDe())) {
-            return new Compte(
-                    nom,
-                    aDateDe,
-                    new Argent(0d, this.getValeur().getDevise())
-            );
-        }
-
+       if(dateDeCreation.isAfter(dateFuture)){
         return new Compte(
-                nom,
-                dateFuture,
-                new Argent(valeur.getMontant(), valeur.getDevise()));
+            nom,
+            dateFuture,
+            ariary(0.0),
+            dateDeCreation);
+       }
+        Argent sommeTrainDeVie = trainDeVie.stream().map(traindeDeVie -> traindeDeVie.projectionFuture(dateFuture)).valeur.reduce(Argent::additionner).orElse(ariary(0))
+        
+        return  new Compte(
+            nom,
+            dateFuture,
+            valeur.soustraire(sommeTrainDeVie),
+            dateDeCreation
+        )
+
+    }
+    public void financer(traindeDeVie trainDeVie){
+        this.traindeDeVie.add(traindeDeVie);
+        return this;
     }
 }
